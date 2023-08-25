66ff3b5cbfe8b45185a3874a21b54b32ff867f37
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-25 19:42:28 +0400

    added User entity

---
+++ src/main/java/com/blbulyandavbulyan/socialmedia/entites/User.java
@@ -0,0 +1,63 @@
+package com.blbulyandavbulyan.socialmedia.entites;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User implements UserDetails {
+    @Id
+    @NotBlank
+    @Size(min = 1, message = "User name too short")
+    @Size(max = 50, message = "User name too long")
+    @Column(nullable = false)
+    private String username;
+    @NotBlank
+    @Column(nullable = false)
+    @Size(min = 10, message = "Password too short")
+    private String password;
+    @NotNull
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
