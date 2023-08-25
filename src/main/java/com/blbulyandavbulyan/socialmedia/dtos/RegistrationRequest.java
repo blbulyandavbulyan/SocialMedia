20c06ecc50fdfd8d6693e59eabc10e00a66a9ab0
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-25 21:09:03 +0400

    marked RegistrationRequest username and password with @ValidUserName and @ValidPassword annotations

--- src/main/java/com/blbulyandavbulyan/socialmedia/dtos/RegistrationRequest.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/dtos/RegistrationRequest.java
@@ -1,16 +1,13 @@
 package com.blbulyandavbulyan.socialmedia.dtos;
 
+import com.blbulyandavbulyan.socialmedia.annotations.validation.ValidPassword;
+import com.blbulyandavbulyan.socialmedia.annotations.validation.ValidUserName;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 public record RegistrationRequest (
-        @Size(min = 1)
-        @Size(max = 50)
-        @NotBlank String username,
-        @Size(min = 10)
-        @NotBlank String password,
+        @ValidUserName String username,
+        @ValidPassword String password,
         @NotNull
         @Email String email
 ){
