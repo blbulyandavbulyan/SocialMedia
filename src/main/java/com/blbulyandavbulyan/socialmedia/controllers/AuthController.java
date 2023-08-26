9debca7e01f4c4c5c291403c62ab861abbc1dc9d
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-26 14:46:26 +0400

    added @AllArgsConstructor in AuthController

--- src/main/java/com/blbulyandavbulyan/socialmedia/controllers/AuthController.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/controllers/AuthController.java
@@ -5,6 +5,7 @@ import com.blbulyandavbulyan.socialmedia.dtos.JwtTokenResponse;
 import com.blbulyandavbulyan.socialmedia.dtos.RegistrationRequest;
 import com.blbulyandavbulyan.socialmedia.services.AuthService;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/")
+@AllArgsConstructor
 public class AuthController {
     private AuthService authService;
     @PostMapping("/register")
