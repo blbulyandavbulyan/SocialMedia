5c218ff79386e2fbc38f2d730f9020a01d1b0b38
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-26 17:10:30 +0400

    fixed @ValidPassword annotation

--- src/main/java/com/blbulyandavbulyan/socialmedia/annotations/validation/ValidPassword.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/annotations/validation/ValidPassword.java
@@ -1,5 +1,7 @@
 package com.blbulyandavbulyan.socialmedia.annotations.validation;
 
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -7,10 +9,13 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+@Constraint(validatedBy = {})
 @NotBlank
-@Size(min = 10, message = "Password too short, must be at least 10 symbols")
-@Size(max = 100, message = "Password too long, maximum length is 100")
+@Size(min = 10, max = 100)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.RECORD_COMPONENT})
 public @interface ValidPassword {
+    String message() default "Password is not valid!";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
