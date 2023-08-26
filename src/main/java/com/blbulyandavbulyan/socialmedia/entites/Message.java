8fb033fbb34ded49cc35712f1b7d5c0c2ede115e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-26 21:55:49 +0400

    added  nullable = false to all columns, added TEXT to column definition for Message#text

--- src/main/java/com/blbulyandavbulyan/socialmedia/entites/Message.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/entites/Message.java
@@ -1,6 +1,7 @@
 package com.blbulyandavbulyan.socialmedia.entites;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -16,14 +17,18 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "message_id")
     private Long id;
+    @NotBlank
+    @Column(name = "text", nullable = false, columnDefinition = "TEXT")
     private String text;
     @ManyToOne
-    @JoinColumn(name = "sender_username")
+    @JoinColumn(name = "sender_username", nullable = false)
     private User sender;
     @ManyToOne
-    @JoinColumn(name = "receiver_username")
+    @JoinColumn(name = "receiver_username", nullable = false)
     private User receiver;
     @CreationTimestamp
+    @Column(name = "sending_date", nullable = false)
     private Instant sendingDate;
+    @Column(name = "read", nullable = false)
     private boolean read;
 }
