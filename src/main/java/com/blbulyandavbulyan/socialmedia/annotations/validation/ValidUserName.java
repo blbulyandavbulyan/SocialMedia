c22ce9e38260a42000da33ec766606b095e7c425
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-26 17:10:47 +0400

    fixed ValidUserName annotation

--- src/main/java/com/blbulyandavbulyan/socialmedia/annotations/validation/ValidUserName.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/annotations/validation/ValidUserName.java
@@ -1,5 +1,7 @@
 package com.blbulyandavbulyan.socialmedia.annotations.validation;
 
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -7,10 +9,13 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-@NotBlank(message = "User name is blank!")
-@Size(min = 1, message = "User name too short, minimum size in symbols is 1")
-@Size(max = 50, message = "User name too long, maximum size in symbols is 50")
+@Constraint(validatedBy = {})
+@NotBlank
+@Size(min = 1, max = 50)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.RECORD_COMPONENT})
 public @interface ValidUserName {
+    String message() default "User name is not valid!";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
