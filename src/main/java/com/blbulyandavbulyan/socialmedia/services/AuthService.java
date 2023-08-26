5be417f52b0bf2638c4f3270d1ef7f6e1c2dbeab
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-26 13:21:56 +0400

    added realization for AuthService#authorize

--- src/main/java/com/blbulyandavbulyan/socialmedia/services/AuthService.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/services/AuthService.java
@@ -1,7 +1,11 @@
 package com.blbulyandavbulyan.socialmedia.services;
 
 import com.blbulyandavbulyan.socialmedia.entites.User;
+import com.blbulyandavbulyan.socialmedia.exceptions.InvalidLoginOrPassword;
+import com.blbulyandavbulyan.socialmedia.utils.JWTTokenUtils;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +14,20 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private PasswordEncoder passwordEncoder;
     private UserService userService;
-
+    private JWTTokenUtils jwtTokenUtils;
     public void registerUser(String username, String password, String email){
         userService.save(new User(username, passwordEncoder.encode(password), email));
     }
     public String authorize(String username, String password){
-        // TODO: 25.08.2023 реализовать метод авторизации, который будет возвращать JWT токен
-        return null;
+        try{
+            UserDetails userDetails = userService.loadUserByUsername(username);
+            if(passwordEncoder.matches(password, userDetails.getPassword())){
+                return jwtTokenUtils.generateToken(userDetails.getUsername(), userDetails.getAuthorities());
+            }
+            else throw new InvalidLoginOrPassword();
+        }
+        catch (UsernameNotFoundException e){
+            throw new InvalidLoginOrPassword();
+        }
     }
 }
