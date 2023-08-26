2b2911dc4c339ba356a712ad6cf9c1b596a31790
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-26 17:18:16 +0400

    Added annotation @ValidUserEmail for email in RegistrationRequest

--- src/main/java/com/blbulyandavbulyan/socialmedia/dtos/RegistrationRequest.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/dtos/RegistrationRequest.java
@@ -1,14 +1,12 @@
 package com.blbulyandavbulyan.socialmedia.dtos;
 
 import com.blbulyandavbulyan.socialmedia.annotations.validation.ValidPassword;
+import com.blbulyandavbulyan.socialmedia.annotations.validation.ValidUserEmail;
 import com.blbulyandavbulyan.socialmedia.annotations.validation.ValidUserName;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 
 public record RegistrationRequest (
         @ValidUserName String username,
         @ValidPassword String password,
-        @NotNull
-        @Email String email
+        @ValidUserEmail String email
 ){
 }
