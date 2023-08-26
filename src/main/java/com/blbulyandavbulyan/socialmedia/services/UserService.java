6398084ab81c5ef67d579e240ea7fe5941ffde82
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-26 11:56:08 +0400

    removed existsByUsername from UserService, added realization for save method

--- src/main/java/com/blbulyandavbulyan/socialmedia/services/UserService.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/services/UserService.java
@@ -2,6 +2,8 @@ package com.blbulyandavbulyan.socialmedia.services;
 
 
 import com.blbulyandavbulyan.socialmedia.entites.User;
+import com.blbulyandavbulyan.socialmedia.exceptions.UserWithThisEmailAlreadyExists;
+import com.blbulyandavbulyan.socialmedia.exceptions.UserWithThisNameAlreadyExist;
 import com.blbulyandavbulyan.socialmedia.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,17 +15,16 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class UserService implements UserDetailsService {
     private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findById(username).orElseThrow(()->new UsernameNotFoundException("User with name " + username + " not found!"));
+        return userRepository.findById(username).orElseThrow(() -> new UsernameNotFoundException("User with name " + username + " not found!"));
     }
-
-    public boolean existByUsername(String username) {
-        // TODO: 25.08.2023 сделать реализацию 
-        return false;
-    }
-
     public void save(User user) {
-        // TODO: 25.08.2023 сделать реализацию
+        if (userRepository.existsByUsername(user.getUsername()))
+            throw new UserWithThisNameAlreadyExist(user.getUsername());
+        if(userRepository.existsByEmail(user.getEmail()))
+            throw new UserWithThisEmailAlreadyExists(user.getEmail());
+        userRepository.save(user);
     }
 }
