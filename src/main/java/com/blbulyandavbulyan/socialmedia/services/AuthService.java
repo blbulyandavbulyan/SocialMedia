bc3a4cb390fba088d28325b6762df87b9c60a369
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-26 11:58:45 +0400

    added realization for AuthService#registerUser

--- src/main/java/com/blbulyandavbulyan/socialmedia/services/AuthService.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/services/AuthService.java
@@ -1,5 +1,6 @@
 package com.blbulyandavbulyan.socialmedia.services;
 
+import com.blbulyandavbulyan.socialmedia.entites.User;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -9,9 +10,9 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private PasswordEncoder passwordEncoder;
     private UserService userService;
+
     public void registerUser(String username, String password, String email){
-        // TODO: 25.08.2023 реализовать метод регистрации пользователя
-        return;
+        userService.save(new User(username, passwordEncoder.encode(password), email));
     }
     public String authorize(String username, String password){
         // TODO: 25.08.2023 реализовать метод авторизации, который будет возвращать JWT токен
