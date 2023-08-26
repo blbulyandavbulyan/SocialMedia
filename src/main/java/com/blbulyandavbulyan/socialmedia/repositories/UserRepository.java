22b9461479ffce16de2a7a2dda722f8d24142280
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-26 11:55:24 +0400

    added existsByUsername method in UserRepository

--- src/main/java/com/blbulyandavbulyan/socialmedia/repositories/UserRepository.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/repositories/UserRepository.java
@@ -2,7 +2,10 @@ package com.blbulyandavbulyan.socialmedia.repositories;
 
 import com.blbulyandavbulyan.socialmedia.entites.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+
 }
