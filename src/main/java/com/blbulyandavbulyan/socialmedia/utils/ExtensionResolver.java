35978bcf22f8e6c8dd4af1ddca426b087123c883
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-27 17:39:35 +0400

    added realization for ExtensionResolver#getFileExtension

--- src/main/java/com/blbulyandavbulyan/socialmedia/utils/ExtensionResolver.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/utils/ExtensionResolver.java
@@ -8,6 +8,11 @@ import java.util.Optional;
 public class ExtensionResolver {
     public Optional<String> getFileExtension(String fileName){
         // TODO: 27.08.2023 реализовать определение расширения файла здесь
+        int dotIndex = fileName.lastIndexOf(".");
+        if(dotIndex != -1) {
+            String extension = fileName.substring(dotIndex);
+            return extension.length() > 1 ? Optional.of(extension) : Optional.empty();
+        }
         return Optional.empty();
     }
 }
