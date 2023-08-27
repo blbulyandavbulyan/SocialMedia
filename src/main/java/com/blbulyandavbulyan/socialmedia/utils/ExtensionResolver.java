e3ebecf53d4642998d97eb05eaf81f766afc4268
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-27 14:18:41 +0400

    added FileExtensionResolver, it

---
+++ src/main/java/com/blbulyandavbulyan/socialmedia/utils/ExtensionResolver.java
@@ -0,0 +1,13 @@
+package com.blbulyandavbulyan.socialmedia.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class ExtensionResolver {
+    public Optional<String> getFileExtension(String fileName){
+        // TODO: 27.08.2023 реализовать определение расширения файла здесь
+        return Optional.empty();
+    }
+}
