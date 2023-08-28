a58064660e723fb794abdd58bc679128da7cf095
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-28 22:57:07 +0400

    added PublicationServiceTest#deletePublicationWhenDeleterIsNotItsOwner and YouAreNotOwnThisPublicationException

--- src/test/java/com/blbulyandavbulyan/socialmedia/services/PublicationServiceTest.java
+++ src/test/java/com/blbulyandavbulyan/socialmedia/services/PublicationServiceTest.java
@@ -1,5 +1,6 @@
 package com.blbulyandavbulyan.socialmedia.services;
 
+import com.blbulyandavbulyan.socialmedia.exceptions.publications.YouAreNotOwnThisPublicationException;
 import com.blbulyandavbulyan.socialmedia.repositories.PublicationRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 public class PublicationServiceTest {
@@ -30,6 +32,18 @@ public class PublicationServiceTest {
         Mockito.when(publicationRepository.existsById(publicationId)).thenReturn(true);
         Mockito.when(publicationRepository.deleteByIdAndAuthorUsername(publicationId, publisherName)).thenReturn(1L);
         assertDoesNotThrow(()->publicationService.delete(publicationId, publisherName));
+        Mockito.verify(publicationRepository, Mockito.times(1)).existsById(publicationId);
+        Mockito.verify(publicationRepository, Mockito.times(1)).deleteByIdAndAuthorUsername(publicationId, publisherName);
+    }
+
+    @Test
+    @DisplayName("delete publication when it exist and deleter is not its owner")
+    void deletePublicationWhenDeleterIsNotItsOwner() {
+        Long publicationId = 1L;
+        String publisherName = "david";
+        Mockito.when(publicationRepository.existsById(publicationId)).thenReturn(true);
+        Mockito.when(publicationRepository.deleteByIdAndAuthorUsername(publicationId, publisherName)).thenReturn(1L);
+        assertThrows(YouAreNotOwnThisPublicationException.class, ()->publicationService.delete(publicationId, publisherName));
         Mockito.verify(publicationRepository, Mockito.times(1)).existsById(publicationId);
         Mockito.verify(publicationRepository, Mockito.times(1)).deleteByIdAndAuthorUsername(publicationId, publisherName);
     }
