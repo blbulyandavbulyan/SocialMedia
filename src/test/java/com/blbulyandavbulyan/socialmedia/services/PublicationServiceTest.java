d144078d41803f648d7999358974b36f7b7064f7
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-28 22:46:26 +0400

    Created PublicationServiceTest and deletePublicationWhenExistsAndDeleterIsItsOwner test

---
+++ src/test/java/com/blbulyandavbulyan/socialmedia/services/PublicationServiceTest.java
@@ -0,0 +1,36 @@
+package com.blbulyandavbulyan.socialmedia.services;
+
+import com.blbulyandavbulyan.socialmedia.repositories.PublicationRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@ExtendWith(MockitoExtension.class)
+public class PublicationServiceTest {
+    @Mock
+    private PublicationRepository publicationRepository;
+    @Mock
+    private UserService userService;
+    @Mock
+    private FileService fileService;
+    @InjectMocks
+    private PublicationService publicationService;
+
+    @Test
+    @DisplayName("delete publication when exists and deleter is its owner")
+    void deletePublicationWhenExistsAndDeleterIsItsOwner() {
+        Long publicationId = 1L;
+        String publisherName = "david";
+        Mockito.when(publicationRepository.existsById(publicationId)).thenReturn(true);
+        Mockito.when(publicationRepository.deleteByIdAndAuthorUsername(publicationId, publisherName)).thenReturn(1L);
+        assertDoesNotThrow(()->publicationService.delete(publicationId, publisherName));
+        Mockito.verify(publicationRepository, Mockito.times(1)).existsById(publicationId);
+        Mockito.verify(publicationRepository, Mockito.times(1)).deleteByIdAndAuthorUsername(publicationId, publisherName);
+    }
+}
