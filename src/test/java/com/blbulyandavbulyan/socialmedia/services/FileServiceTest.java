9cc62e2b928cfeeee49364553c68704b7c04c8a6
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-28 17:38:08 +0400

    Added getFileWhenExistsInDBAndInFileSystem test in FileServiceTest

--- src/test/java/com/blbulyandavbulyan/socialmedia/services/FileServiceTest.java
+++ src/test/java/com/blbulyandavbulyan/socialmedia/services/FileServiceTest.java
@@ -16,9 +16,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.io.Resource;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -43,6 +45,7 @@ class FileServiceTest {
     private ExtensionResolver extensionResolver;
     @InjectMocks
     private FileService fileService;
+
     @Test
     @DisplayName("save valid file")
     public void saveFileWithValidExtensionAndType() throws IOException {
@@ -74,31 +77,34 @@ class FileServiceTest {
         Path filePath = path.resolve(savedFile.getSavedFileName().toString());
         Files.exists(filePath);
     }
+
     @Test
     @DisplayName("save empty file")
     public void saveEmptyFile() throws IOException {
         MockMultipartFile mockMultipartFile = new MockMultipartFile("uploadedFile", InputStream.nullInputStream());
         String publisherName = "testuser";
         Mockito.when(userService.findByUserName(publisherName)).thenReturn(Optional.of(new User()));
-        assertThrows(EmptyFileException.class, ()->fileService.save(mockMultipartFile, publisherName));
+        assertThrows(EmptyFileException.class, () -> fileService.save(mockMultipartFile, publisherName));
         Mockito.verify(fileRepository, Mockito.never()).save(any());
         Mockito.verify(userService, Mockito.only()).findByUserName(publisherName);
         Mockito.verify(fileConfigurationProperties, Mockito.never()).getPath();
     }
+
     @Test
     @DisplayName("save file with invalid mime type")
-    public void saveFileWithInvalidMimeType(){
+    public void saveFileWithInvalidMimeType() {
         String mimeType = "image/jpeg";
         String publisherName = "testuser";
         Mockito.when(userService.findByUserName(publisherName)).thenReturn(Optional.of(new User()));
         MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "test.img", mimeType, new byte[]{2, 3, 4, 5, 6});
-        assertThrows(UploadedFileHasNotAllowedMimeTypeException.class, ()->fileService.save(mockMultipartFile, publisherName));
+        assertThrows(UploadedFileHasNotAllowedMimeTypeException.class, () -> fileService.save(mockMultipartFile, publisherName));
         Mockito.verify(fileConfigurationProperties, Mockito.times(1)).isValidMimeType(mimeType);
         Mockito.verify(fileConfigurationProperties, Mockito.never()).getPath();
     }
+
     @Test
     @DisplayName("save file with invalid extension")
-    public void saveFileWithInvalidExtension(){
+    public void saveFileWithInvalidExtension() {
         String mimeType = "image/jpeg";
         String publisherName = "testuser";
         MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "test.jpg", mimeType, new byte[]{2, 3, 4, 5, 6});
@@ -106,18 +112,45 @@ class FileServiceTest {
         Mockito.when(fileConfigurationProperties.isValidExtension(".jpg")).thenReturn(false);
         Mockito.when(fileConfigurationProperties.isValidMimeType(mimeType)).thenReturn(true);
         Mockito.when(extensionResolver.getFileExtension(mockMultipartFile.getOriginalFilename())).thenReturn(Optional.of(".jpg"));
-        assertThrows(UploadedFileHasInvalidExtensionException.class, ()->fileService.save(mockMultipartFile, publisherName));
+        assertThrows(UploadedFileHasInvalidExtensionException.class, () -> fileService.save(mockMultipartFile, publisherName));
         Mockito.verify(fileRepository, Mockito.never()).save(any());
         Mockito.verify(fileConfigurationProperties, Mockito.never()).getPath();
     }
+
     @Test
     @DisplayName("find all by id")
-    public void findAllById(){
+    public void findAllById() {
         List<File> expected = new ArrayList<>();
         List<UUID> filesUUIDs = new ArrayList<>();
         Mockito.when(fileRepository.findAllById(filesUUIDs)).thenReturn(expected);
         var actual = fileService.findAllById(filesUUIDs);
         Mockito.verify(fileRepository, Mockito.only()).findAllById(filesUUIDs);
         assertSame(expected, actual);
+    }
+
+    @Test
+    @DisplayName("get file when it exists in db and in file system")
+    public void getFileWhenExistsInDBAndInFileSystem() throws IOException {
+        String fileName = "reallycoolimage.jpg";
+        String fileExtension = ".jpg";
+        String contentType = "image/jpeg";
+        UUID savedFileName = UUID.randomUUID();
+        com.blbulyandavbulyan.socialmedia.entites.File file = new File(savedFileName, new User(), fileName, fileExtension, contentType);
+        Mockito.when(fileRepository.findById(savedFileName)).thenReturn(Optional.of(file));
+        Path path = Files.createTempDirectory("socialMediaTmpDir");
+        Mockito.when(fileConfigurationProperties.getPath()).thenReturn(path);
+        Path expectedFilePath = path.resolve(savedFileName.toString());
+        try(FileOutputStream fileOutputStream = new FileOutputStream(expectedFilePath.toString())){
+            fileOutputStream.write(232);
+            fileOutputStream.write(23234);
+        }
+        FileService.FoundFile foundFile = fileService.getFile(savedFileName);
+
+        assertEquals(fileName, foundFile.fileName());
+        assertEquals(contentType, foundFile.contentType());
+        Resource resource = foundFile.resource();
+        assertNotNull(resource);
+        assertEquals(savedFileName.toString(), resource.getFilename());
+        assertEquals(resource.getFile().toPath(), expectedFilePath);
     }
 }
