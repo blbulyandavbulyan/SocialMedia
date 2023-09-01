fad27b213d4e273ef9c7c55e5b40ce3e12791903
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-01 15:22:17 +0400

    For SubscriptionPK added @EqualsAndHashcode annotation

--- src/main/java/com/blbulyandavbulyan/socialmedia/entites/keys/SubscriptionPK.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/entites/keys/SubscriptionPK.java
@@ -1,9 +1,6 @@
 package com.blbulyandavbulyan.socialmedia.entites.keys;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 
@@ -11,6 +8,7 @@ import java.io.Serializable;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class SubscriptionPK implements Serializable {
     private String subscriberUsername;
     private String targetUsername;
