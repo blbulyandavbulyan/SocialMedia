ae324a4a10c8d87b0527be53c29b14c2c805bb69
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-01 11:58:21 +0400

    added SubscriptionRepository#updateViewedBySubscriberAndTargetUsername

--- src/main/java/com/blbulyandavbulyan/socialmedia/repositories/SubscriptionRepository.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/repositories/SubscriptionRepository.java
@@ -3,7 +3,19 @@ package com.blbulyandavbulyan.socialmedia.repositories;
 import com.blbulyandavbulyan.socialmedia.entites.Subscription;
 import com.blbulyandavbulyan.socialmedia.entites.keys.SubscriptionPK;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, SubscriptionPK> {
-    boolean existsBySubscriberUsernameAndTargetUsername(String subscriberName, String subscriptionTargetName);
+    boolean existsBySubscriberUsernameAndTargetUsername(String subscriberName, String targetUsername);
+
+    @Transactional
+    @Modifying
+    @Query("""
+            update Subscription s set s.viewed = :viewed
+            where s.subscriberUsername = :subscriberUsername and s.targetUsername = :targetUsername""")
+    int updateViewedBySubscriberAndTargetUsername(@Param("viewed") Boolean viewed, @Param("subscriberUsername") String subscriberUsername, @Param("targetUsername") String targetUsername);
+
 }
