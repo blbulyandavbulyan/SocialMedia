dae848932ed271f0f1ff645dfec40cdf8a5c983c
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-03 15:26:53 +0400

    added MessageResponseTestImpl

---
+++ src/test/java/com/blbulyandavbulyan/socialmedia/controllers/MessageResponseTestImpl.java
@@ -0,0 +1,34 @@
+package com.blbulyandavbulyan.socialmedia.controllers;
+
+import com.blbulyandavbulyan.socialmedia.dtos.messages.MessageResponse;
+import com.blbulyandavbulyan.socialmedia.entites.Message;
+
+import java.time.Instant;
+
+class MessageResponseTestImpl implements MessageResponse {
+    private final Message m;
+
+    public MessageResponseTestImpl(Message m) {
+        this.m = m;
+    }
+
+    @Override
+    public Long getId() {
+        return m.getId();
+    }
+
+    @Override
+    public String getText() {
+        return m.getText();
+    }
+
+    @Override
+    public boolean isRead() {
+        return m.isRead();
+    }
+
+    @Override
+    public Instant getSendingDate() {
+        return m.getSendingDate();
+    }
+}
