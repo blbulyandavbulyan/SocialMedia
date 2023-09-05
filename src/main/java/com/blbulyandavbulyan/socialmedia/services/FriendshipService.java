91e0bb1ff1abcf84d8256ce5ebd3dcbf99418e4f
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-05 13:11:56 +0400

    removed acceptFriendshipRequest from FriendshipService

--- src/main/java/com/blbulyandavbulyan/socialmedia/services/FriendshipService.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/services/FriendshipService.java
@@ -2,7 +2,6 @@ package com.blbulyandavbulyan.socialmedia.services;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -12,13 +11,6 @@ public class FriendshipService implements IFriendService {
     public boolean areTheyFriends(String first, String second) {
         return subscriptionService.isFirstSubscriberForSecond(first, second)
                 && subscriptionService.isFirstSubscriberForSecond(second, first);
-    }
-
-    @Transactional
-    public void acceptFriendshipRequest(String friendshipAcceptor, String friendshipSender) {
-        subscriptionService.markSubscriptionAsViewed(friendshipAcceptor, friendshipSender);
-        // TODO: 01.09.2023 подумать над тем, должна ли быть эта подписка просмотрена или нет
-        subscriptionService.create(friendshipSender, friendshipSender);
     }
 
 }
