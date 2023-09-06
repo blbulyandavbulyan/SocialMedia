b4d8f2b789852c096843ef6ca44497b289bd702e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-06 14:13:06 +0400

    added SubscriptionRepository#findByTargetUsernameAndViewedIsFalse

--- src/main/java/com/blbulyandavbulyan/socialmedia/repositories/SubscriptionRepository.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/repositories/SubscriptionRepository.java
@@ -1,5 +1,6 @@
 package com.blbulyandavbulyan.socialmedia.repositories;
 
+import com.blbulyandavbulyan.socialmedia.dtos.subcriptions.SubscriptionResponse;
 import com.blbulyandavbulyan.socialmedia.entites.Subscription;
 import com.blbulyandavbulyan.socialmedia.entites.keys.SubscriptionPK;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,8 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, SubscriptionPK> {
 
@@ -17,4 +20,5 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Subs
             where s.subscriberUsername = :subscriberUsername and s.targetUsername = :targetUsername""")
     int updateViewedBySubscriberAndTargetUsername(@Param("viewed") Boolean viewed, @Param("subscriberUsername") String subscriberUsername, @Param("targetUsername") String targetUsername);
 
+    List<SubscriptionResponse> findByTargetUsernameAndViewedIsFalse(String targetUsername);
 }
