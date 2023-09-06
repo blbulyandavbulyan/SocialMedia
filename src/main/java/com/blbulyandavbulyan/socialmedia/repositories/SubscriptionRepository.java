fc87353375a0c73985db8b99adcf7d618e5457fc
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-06 15:47:07 +0400

    Added SubscriptionRepository#findByTargetUsernameAndViewedIsFalse method, it returns Page object now and accepts targetUsername and Pageable, also added SubscriptionRepository#findByTargetUsername with the same accepting parameters, but it returns all subscriptions

--- src/main/java/com/blbulyandavbulyan/socialmedia/repositories/SubscriptionRepository.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/repositories/SubscriptionRepository.java
@@ -1,7 +1,10 @@
 package com.blbulyandavbulyan.socialmedia.repositories;
 
+import com.blbulyandavbulyan.socialmedia.dtos.subcriptions.SubscriptionResponse;
 import com.blbulyandavbulyan.socialmedia.entites.Subscription;
 import com.blbulyandavbulyan.socialmedia.entites.keys.SubscriptionPK;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +20,7 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Subs
             where s.subscriberUsername = :subscriberUsername and s.targetUsername = :targetUsername""")
     int updateViewedBySubscriberAndTargetUsername(@Param("viewed") Boolean viewed, @Param("subscriberUsername") String subscriberUsername, @Param("targetUsername") String targetUsername);
 
+    Page<SubscriptionResponse> findByTargetUsernameAndViewedIsFalse(String targetUsername, Pageable pageable);
+
+    Page<SubscriptionResponse> findByTargetUsername(String targetUsername, Pageable pageable);
 }
