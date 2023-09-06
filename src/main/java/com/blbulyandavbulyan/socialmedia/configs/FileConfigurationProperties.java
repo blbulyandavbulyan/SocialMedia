88786c82802a8e8a9467633f1abb1a5f9ce7e8c7
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-06 11:50:08 +0400

    added FileConfigurationProperties#mimeTypeToValidExtension, and FileConfigurationProperties#getValidExtensionForMimeType, also added properties in application.yaml

--- src/main/java/com/blbulyandavbulyan/socialmedia/configs/FileConfigurationProperties.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/configs/FileConfigurationProperties.java
@@ -5,12 +5,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Optional;
 import java.util.Set;
 
 @Data
 @ConfigurationProperties(prefix = "socialmedia.files")
 @Component
 public class FileConfigurationProperties {
+    private HashMap<String, Set<String>> mimeTypeToValidExtension;
     private Set<String> validExtensions;
     private Set<String> validMimeTypes;
     private Path path;
@@ -19,5 +22,9 @@ public class FileConfigurationProperties {
     }
     public boolean isValidMimeType(String mimeType){
         return validMimeTypes.contains(mimeType);
+    }
+
+    public Optional<Set<String>> getValidExtensionForMimeType(String mimeType) {
+        return Optional.ofNullable(mimeTypeToValidExtension.get(mimeType));
     }
 }
