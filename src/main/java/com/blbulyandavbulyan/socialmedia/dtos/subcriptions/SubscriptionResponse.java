edbebb52d4442f14401ef4f39e74d182dd03c25f
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-06 14:08:37 +0400

    added SubscriptionResponse

---
+++ src/main/java/com/blbulyandavbulyan/socialmedia/dtos/subcriptions/SubscriptionResponse.java
@@ -0,0 +1,6 @@
+package com.blbulyandavbulyan.socialmedia.dtos.subcriptions;
+
+import java.time.Instant;
+
+public record SubscriptionResponse(String subscriberUsername, Instant creationDate, Boolean viewed) {
+}
