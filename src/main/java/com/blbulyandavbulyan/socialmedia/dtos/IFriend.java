4d6b3d54c7997a17d32e5cf21a205698ea4de1e3
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-07 19:20:16 +0400

    added IFriend

---
+++ src/main/java/com/blbulyandavbulyan/socialmedia/dtos/IFriend.java
@@ -0,0 +1,9 @@
+package com.blbulyandavbulyan.socialmedia.dtos;
+
+import java.time.Instant;
+
+public interface IFriend {
+    String getFriendUsername();
+
+    Instant getFriendshipStartDate();
+}
