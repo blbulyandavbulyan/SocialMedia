4f528066b071f2068268078a2e38f689496da453
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-07 19:20:42 +0400

    added SubscriptionRepository#findAllFriends

--- src/main/java/com/blbulyandavbulyan/socialmedia/repositories/SubscriptionRepository.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/repositories/SubscriptionRepository.java
@@ -1,5 +1,6 @@
 package com.blbulyandavbulyan.socialmedia.repositories;
 
+import com.blbulyandavbulyan.socialmedia.dtos.IFriend;
 import com.blbulyandavbulyan.socialmedia.dtos.subcriptions.SubscriptionResponse;
 import com.blbulyandavbulyan.socialmedia.entites.Subscription;
 import com.blbulyandavbulyan.socialmedia.entites.keys.SubscriptionPK;
@@ -23,4 +24,12 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Subs
     Page<SubscriptionResponse> findByTargetUsernameAndViewedIsFalse(String targetUsername, Pageable pageable);
 
     Page<SubscriptionResponse> findByTargetUsername(String targetUsername, Pageable pageable);
+
+    @Query("""
+            select s.subscriberUsername as friendUsername, GREATEST(s.creationDate, s2.creationDate) as friendshipStartDate from Subscription s
+                inner join Subscription s2 on s2.subscriberUsername = s.targetUsername and s2.targetUsername = s.subscriberUsername
+            where s.targetUsername = :target
+            group by s.subscriberUsername, GREATEST(s.creationDate, s2.creationDate)
+            """)
+    Page<IFriend> findAllFriends(@Param("target") String targetUsername, Pageable pageable);
 }
