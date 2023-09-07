f38f41d920073b494015bb6ef3b1e9c1b696f007
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-07 19:21:41 +0400

    added SubscriptionRepositoryTest#findAllFriends

--- src/test/java/com/blbulyandavbulyan/socialmedia/repositories/SubscriptionRepositoryTest.java
+++ src/test/java/com/blbulyandavbulyan/socialmedia/repositories/SubscriptionRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.blbulyandavbulyan.socialmedia.repositories;
 
+import com.blbulyandavbulyan.socialmedia.dtos.Friend;
+import com.blbulyandavbulyan.socialmedia.dtos.IFriend;
 import com.blbulyandavbulyan.socialmedia.dtos.subcriptions.SubscriptionResponse;
 import com.blbulyandavbulyan.socialmedia.entites.Subscription;
 import com.blbulyandavbulyan.socialmedia.entites.User;
@@ -10,8 +12,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -54,5 +55,36 @@ class SubscriptionRepositoryTest {
         assertEquals(1, subscriptionResponsePage.getTotalPages());
         assertTrue(subscriptionResponsePage.hasContent());
         assertTrue(subscriptionResponsePage.getContent().containsAll(subscriptionResponses));
+    }
+
+    @Test
+    void findAllFriends() {
+        User user1 = userRepository.saveAndFlush(new User("user1", "password1234", "user1@example.com"));
+        User user2 = userRepository.saveAndFlush(new User("user2", "password1234", "user2@example.com"));
+        User user3 = userRepository.saveAndFlush(new User("user3", "password1234", "user3@example.com"));
+        User user4 = userRepository.saveAndFlush(new User("user4", "password1234", "user4@example.com"));
+        User user5 = userRepository.saveAndFlush(new User("user5", "password1234", "user5@example.com"));
+        //пусть user1 и user2 взаимно подписаны друг на друга(друзья)
+        Subscription user1ToUser2 = subscriptionRepository.saveAndFlush(new Subscription(user1.getUsername(), user2.getUsername()));
+        Subscription user2ToUser1 = subscriptionRepository.saveAndFlush(new Subscription(user2.getUsername(), user1.getUsername()));
+        Subscription user1ToUser3 = subscriptionRepository.saveAndFlush(new Subscription(user1.getUsername(), user3.getUsername()));
+        Subscription user3ToUser1 = subscriptionRepository.saveAndFlush(new Subscription(user3.getUsername(), user1.getUsername()));
+        subscriptionRepository.saveAndFlush(new Subscription(user4.getUsername(), user1.getUsername()));
+        subscriptionRepository.saveAndFlush(new Subscription(user1.getUsername(), user5.getUsername()));
+        Set<Friend> expected = Set.of(
+                new Friend(user2.getUsername(), max(user2ToUser1.getCreationDate(), user1ToUser2.getCreationDate())),
+                new Friend(user3.getUsername(), max(user3ToUser1.getCreationDate(), user1ToUser3.getCreationDate()))
+        );
+        Page<IFriend> allFriends = subscriptionRepository.findAllFriends(user1.getUsername(), PageRequest.of(0, 4));
+        assertEquals(2, allFriends.getTotalElements());
+        List<Friend> actual = allFriends.getContent().stream().map(iFriend -> new Friend(iFriend.getFriendUsername(), iFriend.getFriendshipStartDate())).toList();
+        assertEquals(2, actual.size());
+        assertEquals(expected.size(), actual.size());
+        assertTrue(expected.containsAll(actual));
+    }
+
+    @SafeVarargs
+    private static <T extends Comparable<T>> T max(T... values) {
+        return Collections.max(Arrays.asList(values));
     }
 }
