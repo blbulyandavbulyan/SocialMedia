2e855c3ce79024e205a88946024d0c0fba92b182
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-09-09 17:13:23 +0400

    replaced qualifier name with import in GlobalControllerAdvice

--- src/main/java/com/blbulyandavbulyan/socialmedia/controllers/advice/GlobalControllerAdvice.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/controllers/advice/GlobalControllerAdvice.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
@@ -15,7 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@org.springframework.web.bind.annotation.ControllerAdvice
+@ControllerAdvice
 public class GlobalControllerAdvice {
     @ExceptionHandler(SocialMediaException.class)
     public ResponseEntity<ApplicationError> processSocialMediaException(SocialMediaException socialMediaException){
