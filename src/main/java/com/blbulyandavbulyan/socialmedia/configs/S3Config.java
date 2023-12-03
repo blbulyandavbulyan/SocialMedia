4ebd231729497a5790b2f5b1a3bee2ba0a56a9a5
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-12-03 23:04:43 +0400

    added S3Config

--- src/main/java/com/blbulyandavbulyan/socialmedia/configs/S3Config.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/configs/S3Config.java
@@ -1,8 +1,12 @@
 package com.blbulyandavbulyan.socialmedia.configs;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3ClientBuilder;
@@ -11,16 +15,23 @@ import java.net.URI;
 
 @Configuration
 @RequiredArgsConstructor
+@Profile("s3")
 public class S3Config {
     private final S3ConfigurationProperties s3ConfigurationProperties;
-
+    @Value("${socialmedia.s3.accessKeyId}")
+    private String accessKeyId;
+    @Value("${socialmedia.s3.secretAccessKey}")
+    private String secretAccessKey;
     @Bean
     public S3Client s3Client() {
         S3ClientBuilder builder = S3Client.builder();
         builder.region(Region.AP_EAST_1);
         String s3Url = s3ConfigurationProperties.getS3Url();
-        if (!s3Url.equals("default"))
+        if (!s3Url.equals("default")) {
             builder.endpointOverride(URI.create(s3Url));
+            builder.forcePathStyle(true);
+        }
+        builder.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretAccessKey)));
         return builder.build();
     }
 }
