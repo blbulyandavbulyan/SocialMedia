d6e14db6bf3f905c2318e5698755bc2715403364
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-12-03 23:05:32 +0400

    now FileService uses FileStorage

--- src/main/java/com/blbulyandavbulyan/socialmedia/services/FileService.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/services/FileService.java
@@ -8,19 +8,15 @@ import com.blbulyandavbulyan.socialmedia.exceptions.files.FileNotFoundException;
 import com.blbulyandavbulyan.socialmedia.exceptions.files.UploadedFileHasInvalidExtensionException;
 import com.blbulyandavbulyan.socialmedia.exceptions.files.UploadedFileHasNotAllowedMimeTypeException;
 import com.blbulyandavbulyan.socialmedia.repositories.FileRepository;
+import com.blbulyandavbulyan.socialmedia.services.filestorage.FileStorage;
 import com.blbulyandavbulyan.socialmedia.utils.ExtensionResolver;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.UUID;
 
@@ -32,7 +28,7 @@ public class FileService {
     private FileRepository fileRepository;
     private UserService userService;
     private ExtensionResolver extensionResolver;
-
+    private FileStorage fileStorage;
     @Transactional
     public UUID save(MultipartFile multipartFile, String publisherName) {
         User uploader = userService.findByUserName(publisherName).orElseThrow();
@@ -46,12 +42,8 @@ public class FileService {
             throw new UploadedFileHasInvalidExtensionException("Uploaded file has invalid extension!");
         File file = new File(UUID.randomUUID(), uploader, multipartFile.getOriginalFilename(), multipartFile.getContentType());
         fileRepository.save(file);
-        try {
-            Files.copy(multipartFile.getInputStream(), fileConfigurationProperties.getPath().resolve(file.getSavedFileName().toString()), StandardCopyOption.REPLACE_EXISTING);
-            return file.getSavedFileName();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        fileStorage.save(multipartFile, file.getSavedFileName());
+        return file.getSavedFileName();
     }
 
     public List<File> findAllById(List<UUID> filesUUIDs) {
@@ -62,18 +54,14 @@ public class FileService {
     }
 
     public FoundFile getFile(UUID fileUIID) {
-        try {
-            Resource resource = new UrlResource(fileConfigurationProperties.getPath().resolve(fileUIID.toString()).toUri());
-            FoundFile foundFile = fileRepository.findById(fileUIID)
-                    .map((file) -> new FoundFile(file.getRealFileName(), file.getMimeType(), resource))
-                    .orElseThrow(() -> new FileNotFoundException("File with " + fileUIID + " not found!"));
-            if(!resource.exists()){
-                log.error("File with UUID '" + fileUIID +"' exists in DB but not found in file system!");
-                throw new FileNotFoundException("File with " + fileUIID + " not found!");
-            }
-            return foundFile;
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
+        Resource resource = fileStorage.getFile(fileUIID);
+        FoundFile foundFile = fileRepository.findById(fileUIID)
+                .map((file) -> new FoundFile(file.getRealFileName(), file.getMimeType(), resource))
+                .orElseThrow(() -> new FileNotFoundException("File with " + fileUIID + " not found!"));
+        if (!resource.exists()) {
+            log.error("File with UUID '" + fileUIID + "' exists in DB but not found in file system!");
+            throw new FileNotFoundException("File with " + fileUIID + " not found!");
         }
+        return foundFile;
     }
 }
