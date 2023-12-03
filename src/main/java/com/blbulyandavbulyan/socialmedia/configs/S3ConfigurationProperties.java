dc39622fa3373ba8eac093d0f6c2b65c68cd400e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-12-03 23:04:24 +0400

    added S3ConfigurationProperties

--- src/main/java/com/blbulyandavbulyan/socialmedia/configs/S3ConfigurationProperties.java
+++ src/main/java/com/blbulyandavbulyan/socialmedia/configs/S3ConfigurationProperties.java
@@ -3,12 +3,14 @@ package com.blbulyandavbulyan.socialmedia.configs;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @Component
 @ConfigurationProperties("socialmedia.s3")
+@Profile("s3")
 public class S3ConfigurationProperties {
     private String s3Url;
     private String bucketName;
